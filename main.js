6a72270e68d531a4b32019fab43b16a0aff8be66
fatah0003 <fatahutd@gmail.com>
2025-04-16 13:48:20 +0200

    commit2

--- main.js
+++ main.js
@@ -1,23 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-// let result = document.querySelector("#ecrand")
-// const btnC = document.querySelector('#btnc')
-// const btnMd = document.querySelector('#btnMd')
-// const btnD = document.querySelector('#btnS')
-// const btn7 = document.querySelector('#btn7')
-// const btn8 = document.querySelector('#btn8')
-// const btn9 = document.querySelector('#btn9')
-// const btnX = document.querySelector('#btnx')
-// const btn4 = document.querySelector('#btn4')
-// const btn5 = document.querySelector('#btn5')
-// const btn6 = document.querySelector('#btn6')
-// const btnM = document.querySelector('#btn-')
-// const btn1 = document.querySelector('#btn1')
-// const btn2 = document.querySelector('#btn2')
-// const btn3 = document.querySelector('#btn3')
-// const btnPl = document.querySelector('#btnPl')
-// const btn0 = document.querySelector('#btn0')
-// const btnV = document.querySelector('#btnv')
-// const btnE = document.querySelector('#btnE')
 
 let result = document.querySelector("#ecrand")
 const boutons = document.querySelectorAll("button");
